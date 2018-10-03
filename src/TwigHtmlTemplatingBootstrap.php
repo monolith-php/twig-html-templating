8288bd46c8fd56c934c8b94faf456cdeb6f6d1af
Shawn McCool <shawn@heybigname.com>
2018-10-03 20:21:34 +0200

    add empty bootstrap

---
+++ src/TwigHtmlTemplatingBootstrap.php
@@ -0,0 +1,16 @@
+<?php namespace Monolith\Twig;
+
+use Monolith\ComponentBootstrapping\ComponentBootstrap;
+use Monolith\DependencyInjection\Container;
+
+final class TwigHtmlTemplatingBootstrap implements ComponentBootstrap
+{
+
+    public function bind(Container $container): void
+    {
+    }
+
+    public function init(Container $container): void
+    {
+    }
+}
