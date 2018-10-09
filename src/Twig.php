d7f4fb2797865ec21ea9aedeb73b4867a7be6744
Shawn McCool <shawn@heybigname.com>
2018-10-09 12:24:49 +0200

    sorta messy for now

--- src/Twig.php
+++ src/Twig.php
@@ -17,4 +17,12 @@ final class Twig
         $template = $this->twig->load($template);
         return $template->render($variables);
     }
+
+    public function renderForm($form, $template, array $variables = []): string
+    {
+        $this->twig->addGlobal('form_model', $form);
+        $template = $this->twig->load($template);
+        
+        return $template->render($variables);
+    }
 }
